f811b5f4f806bc720d54768c52eb249a7fee3bd9
SalTang2099 <2879156336@qq.com>
2024-02-28 12:13:34 -0500

    Update bat, now bat can follow player once being dragged into scene

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -53,7 +53,7 @@ public class Bat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
